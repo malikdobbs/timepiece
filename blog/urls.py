5778bcf80cb833cfb5796f808a076441f912a3da
malikdobbs <malik-dobbs@hotmail.com>
2022-05-26 17:27:58 +0000

    post_detail template added

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.blog_post, name="blog_post"),
-
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
+
 def blog_post(request):
+    """ Allows users to add new blog posts to blog page """
     post_list = Post.objects.filter(status=1).order_by('-created_on')
 
     context = {
@@ -9,3 +11,11 @@ def blog_post(request):
     }
 
     return render(request, 'blog/blog.html', context)
+
+
+def post_detail(request, slug):
+    """ Page allows users to see blog post and leave comments """
+    template_name = 'blog/post_detail.html'
+    post = get_object_or_404(Post, slug=slug)
+
+    return render(request, template_name, {'post': post,})
