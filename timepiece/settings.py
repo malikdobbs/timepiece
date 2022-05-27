306a9d65ba1f91e190e29458c133a2c2b42674ee
malikdobbs <malik-dobbs@hotmail.com>
2022-05-27 13:54:09 +0000

    added toasts

--- timepiece/settings.py
+++ timepiece/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'bag',
     'blog',
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -81,6 +82,8 @@ TEMPLATES = [
         },
     },
 ]
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
