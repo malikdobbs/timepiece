fb2180e821aecaa98bf77e9f59f8c6c8beaf5794
malikdobbs <malik-dobbs@hotmail.com>
2022-05-27 16:41:48 +0000

    added forms, admin and signals

--- checkout/models.py
+++ checkout/models.py
@@ -1,11 +1,11 @@
 import uuid
 
-
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
 from products.models import Product
+
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
@@ -23,19 +23,18 @@ class Order(models.Model):
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
-    def generate_order_number(self):
-        """ 
-        Generate a random, unique order number using UUID 
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
         """
         return uuid.uuid4().hex.upper()
-
 
     def update_total(self):
         """
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
@@ -65,7 +64,7 @@ class OrderLineItem(models.Model):
     def save(self, *args, **kwargs):
         """
         Override the original save method to set the lineitem total
-        and update the order total
+        and update the order total.
         """
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
