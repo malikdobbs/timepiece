79004088ec6ec49b96a37f8aefae470d32ee076e
malikdobbs <malik-dobbs@hotmail.com>
2022-05-27 12:06:39 +0000

    comment section added to blog detail section for users

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
-from . import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('', views.blog_post, name="blog_post"),
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from .forms import CommentForm
 from .models import Post
 
 
@@ -8,6 +9,7 @@ def blog_post(request):
 
     context = {
         'post_list': post_list,
+        'blog_page': 'active',
     }
 
     return render(request, 'blog/blog.html', context)
@@ -17,5 +19,23 @@ def post_detail(request, slug):
     """ Page allows users to see blog post and leave comments """
     template_name = 'blog/post_detail.html'
     post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    # Comment posted
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
 
-    return render(request, template_name, {'post': post,})
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, template_name, {'post': post,
+                                           'comments': comments,
+                                           'new_comment': new_comment,
+                                           'comment_form': comment_form})
