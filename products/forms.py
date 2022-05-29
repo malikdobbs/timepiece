bc978f40ca9c1e3b14b3244b36c81e48cfeda876
malikdobbs <malik-dobbs@hotmail.com>
2022-05-29 11:12:03 +0000

    Product admin view, url and template added

--- products/forms.py
+++ products/forms.py
@@ -3,11 +3,11 @@ from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
-    
+
     class Meta:
         model = Product
         fields = '__all__'
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
